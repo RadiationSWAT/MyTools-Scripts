99824a9e964d7965df9838594448c825a862ef1f
RadiationSWAT <102522858+RadiationSWAT@users.noreply.github.com>
2023-07-31 12:59:09 +0400

    added comments, icon and logo code

--- Python/Python_musicapp/Music_player.py
+++ Python/Python_musicapp/Music_player.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 from pygame import mixer
 import os
 
+#Application GUI
 root=Tk()
 root.title("MusicMuserAI")
 root.geometry("1080x1920")
@@ -11,6 +12,7 @@ root.configure(bg="green")
 root.resizable
 mixer.init()
 
+#Functions for playing and adding music
 def add_music():
     path = filedialog.askdirectory()
     if path:
@@ -27,3 +29,14 @@ def play_music():
     print(Music_Name[0:-4])
     mixer.music.load(Playlist.get(ACTIVE))
     mixer.music.play()
+
+    #Icon & logo
+    Icon_Image = PhotoImage(file="logo.png")
+    root.iconphoto(False, Icon_Image= PhotoImage(file="logo.png"))
+
+    Top_Image = PhotoImage(file="top.png")
+    Label(root, image=Top_Image, bg="green").pack()
+
+    #Logo
+    Logo_Image = PhotoImage(file="logo.png")
+    Label(root, image=Logo_Image, bg="green").place(x=65, y=115)
