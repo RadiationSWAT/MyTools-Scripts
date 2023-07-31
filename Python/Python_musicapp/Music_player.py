ca6c7d3c3354ea113079c433db695be9323255d3
RadiationSWAT <102522858+RadiationSWAT@users.noreply.github.com>
2023-07-31 11:55:04 +0400

    imported libraries to make the app

---
+++ Python/Python_musicapp/Music_player.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import Tk
+from tkinter import filedialog
+from pygame import mixer
+import os
+
+root=Tk()
+root.title("MusicMuserAI")
+root.geometry("1080x1920")
+root.configure(bg="green")
+root.resizable
+mixer.init()
