52837bb144f531efe168660a43ab1cc40732ef8e
RadiationSWAT <102522858+RadiationSWAT@users.noreply.github.com>
2023-07-31 12:51:39 +0400

    added funtions to add music from the dir/created app

--- Python/Python_musicapp/Music_player.py
+++ Python/Python_musicapp/Music_player.py
@@ -10,3 +10,20 @@ root.geometry("1080x1920")
 root.configure(bg="green")
 root.resizable
 mixer.init()
+
+def add_music():
+    path = filedialog.askdirectory()
+    if path:
+        os.chdir(path)
+        songs = os.listdire(path)
+
+        for song in songs:
+            if song.endswith(".mp3"):
+                Playlist.insert(END, song)
+
+
+def play_music():
+    Music_Name= Playlist.get(ACTIVE)
+    print(Music_Name[0:-4])
+    mixer.music.load(Playlist.get(ACTIVE))
+    mixer.music.play()
