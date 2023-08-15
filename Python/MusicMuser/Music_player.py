01ed297b9a42fafdfccffb2c5957abdd2cb6120b
RadiationSWAT <102522858+RadiationSWAT@users.noreply.github.com>
2023-08-15 13:50:41 +0400

    +buttons, +functioning

--- Python/MusicMuser/Music_player.py
+++ Python/MusicMuser/Music_player.py
@@ -43,28 +43,31 @@ Label(root, image=Logo_reduce,bg="#545454").place(x=-60, y=-60)
 
 #Button creation
 Button_prev = PhotoImage(file=os.path.join(os.getcwd(),"Previous.png"))
-prev_reduce = Button_prev.subsample(4)
-Button(root, image=prev_reduce, bg="#545454", bd=0, command=play_music).place(x=10,y=200)
+prev_reduce = Button_prev.subsample(5)
+Button(root, image=prev_reduce, bg="#545454", bd=0, command=mixer.music.queue).place(x=10,y=200)
 
-#Button_next = PhotoImage(file=os.path.join(os.getcwd(),"Next.png"))
-#Button(root, image=Button_next, bg="#0f1a2b", bd=0, command=mixer.music.stop).place(x=30,y=500)
+Button_next = PhotoImage(file=os.path.join(os.getcwd(),"Next.png"))
+next_reduce = Button_next.subsample(5)
+Button(root, image=next_reduce, bg="#545454", bd=0, command=mixer.music.unpause).place(x=250,y=200)
 
-#Button_Resume = PhotoImage(file=os.path.join(os.getcwd(),"Play.png"))
-#Button(root, image=Button_Resume, bg="#0f1a2b", bd=0, command=mixer.music.unpause).place(x=115,y=500)
+Button_Resume = PhotoImage(file=os.path.join(os.getcwd(),"Play.png"))
+play_reduce = Button_Resume.subsample(5)
+Button(root, image=play_reduce, bg="#545454", bd=0, command=play_music).place(x=165,y=200)
 
-#Button_Pause = PhotoImage(file=os.path.join(os.getcwd(),"Pause.png"))
-#Button(root, image=Button_Pause, bg="#0f1a2b", bd=0, command=mixer.music.pause).place(x=200,y=500)
+Button_Pause = PhotoImage(file=os.path.join(os.getcwd(),"Pause.png"))
+pause_reduce = Button_Pause.subsample(5)
+Button(root, image=pause_reduce, bg="#545454", bd=0, command=mixer.music.pause).place(x=90,y=200)
 
 #Music
-#Frame_Music = Frame(root, bd=2 ,relief=RIDGE)
-#Frame_Music.place(x=330,y=350,width=560,height=250)
+Frame_Music = Frame(root, bd=2 ,relief=RIDGE,bg="#83fe9a")
+Frame_Music.place(x=350,y=20,width=270,height=450)
 
-#Button(root,text="Add Music",width=15,height=2,font=("times new roman",12,"bold"),fg="black",bg="#21b3de",command=add_music).place(x=330,y=300)
+Button(root,text="Add Music",width=15,height=2,font=("sans-serif",12,"bold"),fg="black",bg="#83fe9a",command=add_music).place(x=180,y=300)
 
-#Scroll = Scrollbar(Frame_Music)
-#Playlist = Listbox(Frame_Music,width=100,font=("times new roman",10),bg="#333333",fg="grey",selectbackground="lightblue",cursor="hand2",bd=0,yscrollcommand=Scroll.set)
-#Scroll.config(command=Playlist.yview)
-#Scroll.pack(side=RIGHT,fill=Y)
-#Playlist.pack(side=LEFT,fill=BOTH)
+Scroll = Scrollbar(Frame_Music)
+Playlist = Listbox(Frame_Music,width=100,font=("times new roman",10),bg="#83fe9a",fg="#545454",selectbackground="#545454",cursor="hand2",bd=0,yscrollcommand=Scroll.set)
+Scroll.config(command=Playlist.yview)
+Scroll.pack(side=RIGHT,fill=Y)
+Playlist.pack(side=LEFT,fill=BOTH)
 
 root.mainloop()
